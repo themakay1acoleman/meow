09ddb8169d792e2934c0f00501f67a2317af889b
Makayla Coleman <makayla.d.coleman@icloud.com>
2026-03-13 17:37:03 -0500

    lalala

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Header = ({ title = "M E O W" }) => {
+const Header = ({ title = "I Love You" }) => {
     return (
         <header>
             <span className="to-my-love">to my love</span>
@@ -9,9 +9,9 @@ const Header = ({ title = "M E O W" }) => {
                     <img src={`${import.meta.env.BASE_URL}cupid1.png`} alt="cupid" className="header-cupid" />
                     <div>
                         <h1>
-                            <span className="star">♥</span>
+                            <img src={`${import.meta.env.BASE_URL}heart.png`} alt="heart" className="star" style={{height:'1em',verticalAlign:'middle'}} />
                             <span className="meow-text">{title}</span>
-                            <span className="star">♥</span>
+                            <img src={`${import.meta.env.BASE_URL}heart.png`} alt="heart" className="star" style={{height:'1em',verticalAlign:'middle'}} />
                         </h1>
                         <p className="tagline">~ for allen ~</p>
                     </div>
